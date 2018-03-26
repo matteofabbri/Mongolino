54c5a5cb5a424a47651cb89cbe22381fb6f787ad
Matteo Fabbri <matteo@phascode.org>
2018-03-26 14:23:57 +0200

    Added Collection for better support on inheritance

---
+++ Mongolino/ICollectionItem.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Mongolino
+{
+    public interface ICollectionItem
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        string Id { get; set; }
+    }
+}
